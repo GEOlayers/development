5182958ae48e3a4c41dc37c8c94c8a5354130a61
GEOlayers <geolayers.ae@gmail.com>
2021-06-10 23:09:23 +0200

    Delete Autobahn.jsx

--- Autobahn.jsx
+++
@@ -1,8 +0,0 @@
-var myAutobahn = prompt('Bitte Autobahnnummer eingeben A', "");
-var myCountry = prompt('Bitte Land eingeben in dem nach der Autobahn ' + 'A' + myAutobahn + ' gesucht werden soll?', "Deutschland");
-alert('A ' + myAutobahn)
-
-geolayers3.addToBrowser({
-    type:"overpass",
-    query:'[out:json][timeout:25];(node["highway"="motorway"][ref="A ' + myAutobahn + '"]({{bbox}});way["highway"="motorway"][ref="A ' + myAutobahn + '"]({{bbox}});relation["highway"="motorway"][ref="A ' + myAutobahn + '"]({{bbox}}););out body;>;out skel qt;'
-})
