d833b43f628140b30161324dba212cd491492073
GEOlayers <geolayers.ae@gmail.com>
2024-02-12 17:08:04 +0100

    Update BAB.js

--- BAB.js
+++ BAB.js
@@ -5,5 +5,5 @@ var myCountry = prompt('Bitte Land eingeben in dem nach der Autobahn ' + 'A' + m
 geolayers3.addToBrowser({
     type:"overpass",
     // query:'[out:json][timeout:25];{{geocodeArea:' + myCountry + '}}->.searchArea;(node["highway"="motorway"][ref="A ' + myAutobahn + '"](area.searchArea);way["highway"="motorway"][ref="A ' + myAutobahn + '"](area.searchArea);relation["highway"="motorway"][ref="A ' + myAutobahn + '"](area.searchArea););out body;>;out skel qt;'
-    query:'[out:json][timeout:60];{{geocodeArea:' + myCountry + '}}->.searchArea;(nwr["highway"="motorway"][ref="A 24"](area.searchArea););out body;>;out skel qt;'
+    query:'[out:json][timeout:60];{{geocodeArea:' + myCountry + '}}->.searchArea;(nwr["highway"="motorway"][ref="A " + myAutobahn](area.searchArea););out body;>;out skel qt;'
 })
