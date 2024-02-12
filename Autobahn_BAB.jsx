eedefa4046c9b9c6fef6973c9b4264c8ea561c2c
GEOlayers <geolayers.ae@gmail.com>
2024-02-12 17:42:02 +0100

    Update Autobahn_BAB.jsx

--- Autobahn_BAB.jsx
+++ Autobahn_BAB.jsx
@@ -2,11 +2,11 @@ var myAutobahn = prompt('Bitte Autobahnnummer eingeben A', "");
 var myCountry = prompt('Bitte Land eingeben in dem nach der Autobahn ' + 'A' + myAutobahn + ' gesucht werden soll?', "Germany");
 alert('A ' + myAutobahn);
 
-var myQuery = '[out:json][timeout:60];{{geocodeArea:' + '''' + myCountry + '''' + '}}->.searchArea;(nwr["highway"="motorway"][ref="A 4"](area.searchArea););out body;>;out skel qt;';
+var myQuery = '[out:json][timeout:60];{{geocodeArea:' + \'myCountry\' + '}}->.searchArea;(nwr["highway"="motorway"][ref="A 4"](area.searchArea););out body;>;out skel qt;';
 alert("das ist mein query: " + myQuery);
 
 geolayers3.addToBrowser({
     type:"overpass",
     // query:'[out:json][timeout:25];{{geocodeArea:' + myCountry + '}}->.searchArea;(node["highway"="motorway"][ref="A ' + myAutobahn + '"](area.searchArea);way["highway"="motorway"][ref="A ' + myAutobahn + '"](area.searchArea);relation["highway"="motorway"][ref="A ' + myAutobahn + '"](area.searchArea););out body;>;out skel qt;'
-    query:'[out:json][timeout:60];{{geocodeArea:' + 'Germany' + '}}->.searchArea;(nwr["highway"="motorway"][ref="A 4"](area.searchArea););out body;>;out skel qt;'
+    query:'[out:json][timeout:60];{{geocodeArea:' + \'myCountry\' + '}}->.searchArea;(nwr["highway"="motorway"][ref="A 4"](area.searchArea););out body;>;out skel qt;'
 })
