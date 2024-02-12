b75b25ab363e8c3491d88375d360ed108abde88f
GEOlayers <geolayers.ae@gmail.com>
2024-02-12 18:06:51 +0100

    Update Autobahn_BAB.jsx

--- Autobahn_BAB.jsx
+++ Autobahn_BAB.jsx
@@ -1,7 +1,7 @@
 var myAutobahn = prompt('Bitte Autobahnnummer eingeben A', "");
 var myCountry = prompt('Bitte Land eingeben in dem nach der Autobahn ' + 'A' + myAutobahn + ' gesucht werden soll?', "Germany");
 myCountry = "\'" + myCountry + "\'";
-alert(myCountry);
+alert("so sieht das jetzt aus: " + myCountry);
 //alert('A ' + myAutobahn)
 
 geolayers3.addToBrowser({
